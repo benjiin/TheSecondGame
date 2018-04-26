995bf8810f36cfd9f77152cb237b0c0839fc5ba2
CBM\benjaminr <benjamin.rosenkranz@gmail.com>
2018-04-26 10:52:39 +0200

    Projektdateien hinzufügen.

---
+++ TheSecondGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheSecondGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
